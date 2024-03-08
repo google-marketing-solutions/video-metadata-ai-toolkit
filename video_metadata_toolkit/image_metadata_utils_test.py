efc3305e96a8cfb0538ea3fb8ce72a00cba1ec3a
Chris Feldman <cfeldman@google.com>
2024-03-08 19:41:27 +0000

    Mock out ffmpeg, cloud.vision, open in image_metadata_utils_test.
    Also, formatted effected files and fixed some linting issues.
    
    Change-Id: I09e60a376ad6f3f0f7fe416dc87bbc4a4947872d

--- video_metadata_toolkit/image_metadata_utils_test.py
+++ video_metadata_toolkit/image_metadata_utils_test.py
@@ -11,36 +11,49 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Tests for image_metadata_utils."""
 
 import unittest
+from unittest import mock
 
 import ffmpeg
 import image_metadata_utils as utils
+from google import cloud
 
 
 class TestImageMetadataUtils(unittest.TestCase):
 
-  def test_detect_labels_dict_bad_path(self):
+  @mock.patch("builtins.open")
+  @mock.patch.object(cloud, "vision", autospec=True)
+  def test_detect_labels_dict_bad_path(self, _, mock_open):
     "Tests for valid screenshots to exist before processing."
 
     path = "BAD_PATH"
     threshold = 0.70
     in_dict = {}
+    mock_open.side_effect = FileNotFoundError()
+
     with self.assertRaises(FileNotFoundError):
       utils.detect_labels_dict(path, in_dict, threshold)
 
-  def test_generate_screenhot_bad_path(self):
+  @mock.patch.object(ffmpeg, "input", autospec=True)
+  def test_generate_screenhot_bad_path(self, mock_ffmpeg_input):
     "Tests for valid video file to exist before processing."
-
     in_filename = "BAD_PATH"
     out_filename = "test.jpg"
     time = 10
     width = 500
+    mock_ffmpeg_input.side_effect = ffmpeg.Error(
+        "cmd",
+        "stdout",
+        mock.MagicMock(),
+    )
 
     with self.assertRaises(ffmpeg.Error):
       utils.generate_screenshot(in_filename, out_filename, time, width)
 
-  def test_gen_video_duration_bad_path(self):
+  @mock.patch.object(ffmpeg, "input", autospec=True)
+  def test_gen_video_duration_bad_path(self, _):
     "Tests for valid video file to exist before processing."
 
     in_filename = "BAD_PATH"
